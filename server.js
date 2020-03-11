e5e4f6419286f1302de7c8e0eb4d204ee6d332f1
yudit12 <yudit12@users.noreply.github.com>
2020-03-11 19:58:39 +0200

    adding port for Heroku deployment

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ app.put('/image',(req,res)=>{image.handleImage(req, res,db)})
 
 app.post('/imageUrl',(req,res)=>{image.hanleApiCall(req, res)})
 
-app.listen(3000,()=>{
-    console.log('app is running on port 3000')
+app.listen(process.env.PORT ||3000,()=>{
+    console.log(`app is running on port ${process.env.PORT}`)
 });
 
