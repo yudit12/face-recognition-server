a2ada2d6524bb8bfb5c204fbf6009d86c655cec5
yudit12 <yudit12@users.noreply.github.com>
2020-06-24 01:51:52 +0300

    Update __init__.py

--- api/__init__.py
+++ api/__init__.py
@@ -11,8 +11,8 @@ def create_app():
     app = Flask(__name__, static_folder="C:/Users/use/Documents/GitHub/face-recognition/build", static_url_path='/')
     # bcrypt = Bcrypt(app)
     # app.config['SECRET_KEY']='b726da71426e92068ed89850620b1522'
-    ENV = 'dev'
-    # ENV = 'prod'
+    # ENV = 'dev'
+    ENV = 'prod'
     if ENV == 'dev':
 
         app.debug=True
