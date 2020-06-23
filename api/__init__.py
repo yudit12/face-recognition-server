b85d575dbbd8834d98575df513258a3fdefb2d91
yudit12 <yudit12@users.noreply.github.com>
2020-06-24 02:52:05 +0300

    d

--- api/__init__.py
+++ api/__init__.py
@@ -3,14 +3,13 @@
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-from flask_cors import CORS 
+
 # from flask_bcrypt import Bcrypt
 db = SQLAlchemy()
 ma = Marshmallow()
 def create_app():
     #, static_folder="C:/Users/use/Documents/GitHub/face-recognition/build", static_url_path='/'
     app = Flask(__name__)
-    CORS(app)
     # bcrypt = Bcrypt(app)
     # app.config['SECRET_KEY']='b726da71426e92068ed89850620b1522'
     # ENV = 'dev'
